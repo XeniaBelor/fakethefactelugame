1fba48bc48b211dc858dcac503d9a28c6a74ac84
XeniaBelor <belorgames@gmail.com>
2019-03-29 00:41:16 +0200

    passwords.js sepparetly folder

--- main.php
+++ main.php
@@ -35,20 +35,6 @@
 </form> 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-$(document).ready(function() {
-$("#password2").keyup(validate);
-});
-
-function validate() {
-var password1 = $("#password1").val();
-var password2 = $("#password2").val();
-  if(password1 == password2) {
-     $("#validate-status").text("Passwords match!");        
-  }
-  else {
-      $("#validate-status").text("Password do not match!");  
-  }
-}  </script>
+<script src="js/passwords.js"></script>
 </body>
 </html>
